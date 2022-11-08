574d540b5f7007b08768213a267963fdb00cb281
Erwange <erwangebis@gmail.com>
2022-11-08 08:36:01 +0100

    Continuité Playfab
    Début de Login
    Lien entre Unity et Visual établi => Autocompletion

---
+++ Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+
+public class PlayfabManager : MonoBehaviour
+{
+    [Header("UI")]
+    public Text messageText;
+    public InputField emailInput;
+    public InputField passwordInput;
+
+
+
+    public void Register()
+    {
+
+    }
+
+    public void Login()
+    {
+
+    }
+
+    public void ResetPassword()
+    {
+
+    }
+}
